8c8a57f5eba3b4daa4cdbeb475e7f64aeb978835
tjaderIMM <130658502+tjaderIMM@users.noreply.github.com>
2024-06-26 13:37:57 +0200

    Add files via upload

--- js/calculator.js
+++ js/calculator.js
@@ -267,7 +267,7 @@
 					// 	' raz[y]</b> ' + bootonMenu;
 					= '<b id="b">Średnio co ' + "<br>" + Math.round(result) + '</b>' + "<br>" + stat + osoba + " w wieku" + getAgeText2() +'<br>' + contact +"mieć kontakt z informacją o kampanii " + bootonMenu;
 			} else {
-				text = (mchecked ? 'Statystyczny ' : 'Statystyczna ') + person_i18n + ' w wieku' + getAgeText2() + '<br>' + (mchecked ? ' mógł ' : ' mogła ') + 'mieć kontakt z informacją o kampanii<br><b id="b">średnio <br>' + result.toFixed(2) + ' raz[y]</b>' + bootonMenu;
+				text = (mchecked ? 'Statystyczny ' : 'Statystyczna ') + person_i18n + ' w wieku' + getAgeText2() + '<br>' + (mchecked ? ' mógł ' : ' mogła ') + 'mieć kontakt z informacją o kampanii<br><b id="b">średnio <br>' + Math.round(result)+ ' raz[y]</b>' + bootonMenu;
 			};
 
 			messageCtn.hide().html(text).fadeIn('slow');
